0b354c086dde6d6c978f1bd1fed8ddd82f3b1411
Fernanda Valenzuela <fe.valenzuelac@duocuc.cl>
2023-05-12 02:18:08 -0400

    se agregan overlays, funciones js y galerías de img

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,18 +1,89 @@
+$.getScript("assets/js/script_key.js", function() {
+    getWeatherMapKey();
+})
+
+$.getScript("assets/js/script_key.js", function() {
+    getMarvelKey();
+})
+
+// Modo dark
+
 const darkMode = () => {
-    document.querySelector("body").setAttribute("data-bs-theme", "dark")
-    document.querySelector("#tema-icon").setAttribute("class", "bi bi-sun-fill")
+    $("body").attr("data-bs-theme", "dark");
+    $("#tema-icon").attr("class", "bi bi-sun-fill");
 }
 
 const lightMode = () => {
-    document.querySelector("body").setAttribute("data-bs-theme", "light")
-    document.querySelector("#tema-icon").setAttribute("class", "bi bi-moon-fill")
+    $("body").attr("data-bs-theme", "light");
+    $("#tema-icon").attr("class", "bi bi-moon-fill");
 }
 
 const cambiarTema = () => {
-    document.querySelector("body").getAttribute("data-bs-theme") === "light"?
+    $("body").attr("data-bs-theme") === "light"?
     darkMode() : lightMode();
 }
 
 $("#btn-theme").on('click', function (){
     cambiarTema();
 })
+
+function mostrarFechaActual(){
+    var today = new Date();
+    var options = {weekday: 'short', day: 'numeric', month: 'short', year: 'numeric'};
+
+    var fecha = today.toLocaleString('es', options);
+    var hora = today.toLocaleTimeString('es');
+    $('#fecha').html(fecha);
+    $('#hora').html(hora);
+    let tiempo = setTimeout(function() {
+        mostrarFechaActual()
+    }, 1000);
+}
+
+
+
+function clima(){
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            let units = 'metric';
+            let appid = getWeatherMapKey();
+            let urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${appid}`
+            $.getJSON(urlClima, function(dataClima) {
+                let icon_url = `https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`;
+                let html_img = `<img id="icon-clima" src="${icon_url}" alt="${dataClima.weather[0].description}"/>`;
+                let html_clima = `<p id="temperatura" class="text-center">${dataClima.main.temp}°C</p>`;
+                
+                $('#info-clima').html(html_clima);
+                $('#img-clima').html(html_img);
+            });
+        });
+        
+    
+    
+    } else {
+    /* geolocation IS NOT available */
+    }
+    
+}
+
+function myMap() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            const myLatLng = { lat: lat, lng: lon};
+            var mapProp= {
+            center:myLatLng,
+            zoom:16,
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            var marker = new google.maps.Marker({position: myLatLng, map,});
+
+            marker.setMap(map);
+        });
+    }
+}
+
+
---
+++ assets/js/script_key.js
@@ -0,0 +1,9 @@
+function getWeatherMapKey() {
+    const appid = '7e9a3edffcc7742bf9008a5c0b760435';
+    return appid;
+}
+
+function getMarvelKey() {
+    const key = 'fe6e42d2c291799e1571d5dce4c91eaa';
+    return key;
+}
