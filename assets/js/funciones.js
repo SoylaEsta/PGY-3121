1eb4f64671b016e83cedd2e7595a1ef37578473e
Fernanda Valenzuela <fe.valenzuelac@duocuc.cl>
2023-05-15 19:03:55 -0400

    se agrega formulario de contacto

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -31,8 +31,11 @@ function mostrarFechaActual(){
 
     var fecha = today.toLocaleString('es', options);
     var hora = today.toLocaleTimeString('es');
-    $('#fecha').html(fecha);
-    $('#hora').html(hora);
+
+    var html_hora = `<p id="card-title" class="text-center my-0">${hora}</p>`;
+    var html_fecha = `<p id="card-text" class="text-center ">${fecha}</p>`;
+    $('#fecha').html(html_fecha);
+    $('#hora').html(html_hora);
     let tiempo = setTimeout(function() {
         mostrarFechaActual()
     }, 1000);
@@ -45,15 +48,19 @@ function clima(){
         navigator.geolocation.getCurrentPosition(position => {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
+            let lang = 'es';
             let units = 'metric';
             let appid = getWeatherMapKey();
-            let urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${appid}`;
+            let urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&lang=${lang}&appid=${appid}`;
             $.getJSON(urlClima, function(dataClima) {
                 let icon_url = `https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`;
-                let html_img = `<img id="icon-clima" src="${icon_url}" alt="${dataClima.weather[0].description}"/>`;
-                let html_clima = `<p id="temperatura" class="text-center">${dataClima.main.temp}°C</p>`;
-                
-                $('#info-clima').html(html_clima);
+                let desc_clima = dataClima.weather[0].description;
+                let html_img = `<img id="icon-clima" class="mx-1" src="${icon_url}" alt="${dataClima.weather[0].description}"/>`;
+                let html_temp = `<p class="text-center mb-0">${dataClima.main.temp}°C</p>`;   
+                let html_desc = `<p class="text-center">${desc_clima}</p>`;
+
+                $('#temperatura').html(html_temp);
+                $('#descripcion-clima').html(html_desc);
                 $('#img-clima').html(html_img);
             });
         });
@@ -88,7 +95,20 @@ $.getJSON(`https://rickandmortyapi.com/api/character/2`, function(data){
     let nombre = data.name;
     let img = data.image;
     let html_img = `<img src="${img}" class="img-fluid rounded-start" alt="..."></img>`;
-   
-    $('#poke-1').html(html_img);
-    $('#poke-title-1').html(nombre);
+    let url_episode = data.episode[1];
+    let descripcion =
+    `
+    <p id="card-text-1" class="card-text">Status: ${data.status}</p>
+    <p id="card-text-1" class="card-text">Species: ${data.species}</p>
+    <p id="card-text-1" class="card-text">Gender: ${data.gender}</p>
+    <p id="card-text-1" class="card-text">Origin: ${data.location.name}</p>
+    `;
+    $.getJSON(url_episode, function(episode){
+        let eps_name = episode.name;
+        console.log(eps_name);
+    });
+    
+    $('#img-1').html(html_img);
+    $('#card-title-1').html(nombre);
+    $('#card-text-1').html(descripcion);
 });
